d463d666af4c8a15c3291705362b42340d9db5a3
Develov <beniro_vielma@hotmail.com>
2018-09-27 14:57:03 -0500

    first commit of app profile to heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var path = require("path");
 
 app.use(bodyparser.urlencoded({extends: false}));
 app.use(bodyparser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 app.get('/', (req, res) =>
 	{
 		res.sendFile(path.join(__dirname+'/build/index.html'));
